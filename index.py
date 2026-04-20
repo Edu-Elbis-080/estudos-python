91958d6142025ad8bda832012c259b81712e536e
Edu-Elbis-080 <edu.gabrielpin@gmail.com>
2026-04-20 18:59:48 -0300

    dia 2 estudando

--- index.py
+++ index.py
@@ -1,2 +1,12 @@
 print("hello world")
 print("thank yuo")
+print(3/5)
+print(10*20)
+print(4**2)
+print('eai' + str(4*4))
+print("ola" + str(-990))
+
+numero = int(input("digite um numero: "))
+print("voce digitou:", numero)
+
+3+3*64
